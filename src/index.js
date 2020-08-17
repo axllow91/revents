231965b7b423cef2592d56ace531328c5dfadf1f
Marian Dobre <dobremi91@gmail.com>
2020-08-17 16:43:30 +0300

    first app commit message

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEl
+  );
+}
+
+// if we made a change
+// and the hot module is available
+// it's going to take the app comparator and replace on our page App
+// without doing a full page reload
+if(module.hot) {
+  module.hot.accept('./app/layout/App', function() {
+    setTimeout(render);
+  });
+}
+
+render();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
